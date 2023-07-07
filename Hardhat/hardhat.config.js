e086dc75ad7bf969cc2d0a6e8e5e3334c0a6932e
holisticode <holistic.computing@gmail.com>
2023-07-07 17:56:07 -0500

    local blockscout hardhat config

--- Hardhat/hardhat.config.js
+++ Hardhat/hardhat.config.js
@@ -6,35 +6,46 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const API_KEY = process.env.API_KEY;
 
 module.exports = {
-  solidity: '0.8.17',
-  settings: {
-    optimizer: {
-      enabled: true,
-      runs: 200
-    }
-  },
-  networks: {
-    mainnet: {
-      url: `https://rpcapi.fantom.network`,
-      chainId: 250,
-      accounts: [`0x${PRIVATE_KEY}`]
-    },
-    testnet: {
-      url: `https://rpc.testnet.fantom.network`,
-      chainId: 4002,
-      accounts: [`0x${PRIVATE_KEY}`]
-    },
-    coverage: {
-      url: 'http://localhost:8555'
-    },
-    localhost: {
-      url: `http://127.0.0.1:8545`
-    }
-  },
-  etherscan: {
-    apiKey: {
-      ftmTestnet: API_KEY,
-      opera: API_KEY
-    }
-  }
+	solidity: '0.8.17',
+	settings: {
+		optimizer: {
+			enabled: true,
+			runs: 200
+		}
+	},
+	networks: {
+		mainnet: {
+			url: `https://rpcapi.fantom.network`,
+			chainId: 250,
+			accounts: [`${PRIVATE_KEY}`]
+		},
+		testnet: {
+			url: `https://rpc.testnet.fantom.network`,
+			chainId: 4002,
+			accounts: [`${PRIVATE_KEY}`]
+		},
+		coverage: {
+			url: 'http://localhost:8555'
+		},
+		fakenet: {
+			url: `http://127.0.0.1:4000`
+		}
+	},
+	etherscan: {
+		apiKey: {
+			ftmTestnet: API_KEY,
+			opera: API_KEY,
+			fakenet: API_KEY
+		},
+		customChains: [
+			{
+				network: 'fakenet',
+				chainId: 4003,
+				urls: {
+					apiURL: 'http://localhost:4444/api',
+					browserURL: 'http://localhost:4444/',
+				},
+			},
+		],
+	}
 };
